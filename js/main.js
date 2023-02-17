ba253822e79a6542bd28c130b166f2ac5c17e370
nemer-jarada <object4life@gmail.com>
2023-02-17 09:48:28 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -11,20 +11,71 @@ $(document).ready(function () {
             $('header nav .navbar-brand').css('color', 'black')
         }
     });
-    // owl-carousel
 
-    $('.owl-carousel').owlCarousel({
+    // owl-carousel
+    $('.hero-carousel').owlCarousel({
         items: 1,
         loop: true,
         mouseDrag: false,
         touchDrag: false,
         animateOut: 'fadeOut',
         autoplay: true,
-        autoplayTimeout: 5000,
+        autoplayTimeout: 4000,
         dots: false
-
     })
 
+    $(".testim-carousel").owlCarousel({
+        autoplay: true,
+        autoplayTimeout: 3000,
+        smartSpeed: 1500,
+        dots: false,
+        loop: true,
+        center: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            }
+        }
+    });
+    $(".brand-carousel").owlCarousel({
+        autoplay: true,
+        autoplayTimeout: 2000,
+        smartSpeed: 1500,
+        dots: false,
+        loop: true,
+        responsive: {
+            0: {
+                items: 2
+            },
+            576: {
+                items: 3
+            },
+            768: {
+                items: 5
+            },
+            992: {
+                items: 8
+            }
+        }
+    });
 
+
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 200 && $(window).width() > 768) {
+            $('.box').addClass('animi')
+        }
+    });
+
+    new WOW().init();
 
 });
